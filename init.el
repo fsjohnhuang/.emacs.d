1e83db44cb42a2e4dd2967c8556bfbdd2500f53f
fsjohnhuang <fsjohnhuang@hotmail.com>
2018-11-19 14:47:42 +0800

    Add init-better-defaults.el

---
+++ init.el
@@ -0,0 +1,4 @@
+;;; -*- lexical-binding: t -*-
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+(require 'init-better-defaults)
---
+++ lisp/init-better-defaults.el
@@ -0,0 +1,56 @@
+;;; -*- lexical-binding: t -*-
+
+;; ---- 个人信息 ----
+(setq user-full-name "John Huang")
+(setq user-email-address "fsjohnhuang@hotmail.com")
+;; ---- END 个人信息 ----
+
+;; ---- 窗口界面 ----
+(tool-bar-mode nil)
+(menu-bar-mode nil)
+(scroll-bar-mode nil)
+(set-scroll-bar-mode nil)
+;; ---- END 窗口界面 ----
+
+;; ---- 编辑 ----
+;; 行号、列号
+(column-number-mode t)
+(line-number-mode t)
+(global-linum-mode t)
+(setq linum-format "%d ")
+
+(mouse-avoidance-mode 'animate) ;; 光标靠近鼠标时，鼠标隐藏
+(global-font-lock-mode t) ;; 语法高亮
+(global-hl-line-mode t) ;; 当前行高亮
+
+;; 括号、双引号自动匹配/补全
+(show-paren-mode t)
+(electric-pair-mode t)
+
+;; 设置tab键
+(setq-default indent-tabs-mode nil)
+(setq default-tab-width 4)
+(setq tab-stop-list ())
+
+(delete-selection-mode t) ;; 对于选中区的文本，采用先删除后录入的方式
+
+;; 文件自动保存和备份
+(setq auto-save-file nil)
+(setq make-backup-files nil)
+
+(global-auto-revert-mode t) ;; 文件发生变化则自动更新缓冲区
+
+(setq scroll-margin 3 scroll-conservatively 10000) ;; 光标靠近上下边3行开始滚动屏幕
+(fset 'yes-or-no-p 'y-or-n-p) ;; 缩写二次确认提示信息
+;; ---- END 编辑 ----
+
+;; ---- 增强 ----
+;; 格式化整个缓冲区
+(defun indent-buffer ()
+  (interactive)
+  (save-excursion
+    (indent-region (point-min) (point-max) nil)))
+;; ---- END 增强 ----
+
+
+(provide 'init-better-defaults)
